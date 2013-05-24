be0e75bc22999547bec54a2af8dd9089d48619fc
James <james@finedesigngroup.com>
2013-05-24 10:32:24 -0700

    update vhost reading logic

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 ## Goals
 
 The goal behind the IP Auditor is to be an installable Gem that runs locally with SSH credentials to a server. It then returns a list of all domains the server is listening for and what IP the domains are acutally pointing to.
-
--- lib/ip_auditor.rb
+++ lib/ip_auditor.rb
@@ -1,12 +1,27 @@
 # require "ip_auditor/version"
-require "uri"
 
 module IpAuditor
-  domain_text = `grep -r 'ServerName' /etc/apache2/vhosts`
+  # grab domains from vhosts
+  domain_text = `grep -r 'ServerName\\|ServerAlias' /etc/apache2/vhosts`
+  # pull domains out of grep results
   domains = domain_text.scan(/(ServerName|ServerAlias)\s*?(.+)/i).to_a
   domains.each do |domain|
+    # strip domains of whitespace
     d = domain[1].strip
-    output = `nslookup #{d}`
-    puts output
+    # perform an nslookup
+    lookup = `nslookup #{d}`
+
+    # strip output to IPs only
+    output = lookup.scan(/(Non-authoritative answer:)(.*)/m).to_a
+    ips = output[0][1].scan(/Address: (.*)/).to_a if output[0]
+
+    # output results
+    puts d
+    if ips
+      puts ips
+    else
+      puts 'NO IP FOUND'
+    end
+    puts '====='
   end
 end
